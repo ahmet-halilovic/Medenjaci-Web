b2bdad250984f4f04a46d73c81cfc76cff5e8f22
Ahmet <ahalilovic@i4di.org>
2021-01-03 15:58:13 +0100

    Add breadcrumbs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProductDetailsComponent} from './components/shop/product-details/product-details.component';
+import {ProductPageComponent} from './components/shop/product-page/product-page.component';
 
 const routes: Routes = [
-  {path: '', component: ProductDetailsComponent}
+  {path: '', component: ProductPageComponent}
 ];
 
 @NgModule({
